1558166c8071d4b062e22129a9902ed17d9d2a83
Lexxicon <dolorosul@gmail.com>
2017-04-14 18:02:06 -0500

    added constant defs

---
+++ core/src/main/java/com/lexxiconstudios/vestibule/core/Constants.java
@@ -0,0 +1,6 @@
+package com.lexxiconstudios.vestibule.core;
+
+public interface Constants {
+	public static final short CATEGORY_ENV  = 0x0001;
+	public static final short CATEGORY_PLAYER = 0x0002;
+}
